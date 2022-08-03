a75d05e94ddccdb628406eb7cf02b07406eefb3a
Marie <mcari1311@gmail.com>
2022-08-03 18:40:04 -0400

    I can view a list of all my pokemon.

--- views/Index.jsx
+++ views/Index.jsx
@@ -9,7 +9,18 @@ class Index extends React.Component {
     render() {
         const { pokemon } = this.props
         return (
-            <h1 style={myStyle}>See all the Pokemon!</h1>
+            <div> 
+                <h1 style={myStyle}>See all the Pokemon!</h1>
+                {pokemon.map((pokemon, i) => {
+                   return (
+                       <li>
+                           <a href={`/pokemon/${i}`}>
+                               {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) }
+                           </a>
+                       </li>
+                   )
+               })}
+            </div>
         )
     }
 }
