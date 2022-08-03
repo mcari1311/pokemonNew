10b7fdc684d4d23bf6772fd556bcc9e5e14f3116
Marie <mcari1311@gmail.com>
2022-08-03 18:07:27 -0400

    All my files are created.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express') 
+require('dotenv').config()
+const app = express()  
+const port = process.env.PORT || 3003
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.listen(3000, function() {
+    console.log('Listening on port', port)
+})
