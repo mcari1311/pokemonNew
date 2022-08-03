5d510aedd479c47e9690362c440ca311fac2b436
Marie <mcari1311@gmail.com>
2022-08-03 18:12:22 -0400

    My server is set up and running.

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3003
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Pokemon App!');
+});
+
 app.listen(3000, function() {
     console.log('Listening on port', port)
 })
