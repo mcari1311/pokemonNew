fd0e9dbb491380c205be806e6e0eb60a757399bc
Marie <mcari1311@gmail.com>
2022-08-03 18:19:42 -0400

    Connected my database, can see json in browser

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const port = process.env.PORT || 3003
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+const pokemon = require('./models/pokemon.js')
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
 });
-
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
+})
 app.listen(3000, function() {
     console.log('Listening on port', port)
 })
